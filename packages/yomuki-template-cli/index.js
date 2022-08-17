893fea68f9f731c233f7b91be29607cde8a52595
zengrujing <yomuki@qq.com>
2022-08-17 18:00:15 +0800

    init

---
+++ packages/yomuki-template-cli/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import create from "./lib/create.js";
+import fs from "node:fs";
+const program = new Command();
+// 读取package
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"));
+
+// 设置cli版本 -V
+program.version(pkg.version);
+
+program.command("create").description("从模板创建项目").action(create);
+program.parse(process.argv);
